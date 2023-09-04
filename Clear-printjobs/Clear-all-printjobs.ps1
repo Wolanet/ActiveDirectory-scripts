1122cfda2eca44fd217cb41b644511dffe10d7db
Lorenzo <139185774+Wolanet@users.noreply.github.com>
2023-09-04 02:52:05 +0200

    Add files via upload

---
+++ Clear-printjobs/Clear-all-printjobs.ps1
@@ -0,0 +1,18 @@
+﻿
+# Removes all the print jobs from all the printer devices, useful if your printer/print spooler is stuck and you need to reset it or clear the Queue
+
+	#get a list of all the printers in AD, also considering if you don't find any printers
+	$printers = Get-Printer
+	if ($printers.Count -eq 0) { throw "No printers available" }
+	
+	# Loop through each printer and remove all print jobs
+	foreach ($printer in $printers) {
+		$printerName = $printer.Name
+        Write-Host "Cancelling print jobs for printer: $printerName"
+		$printjobs = Get-PrintJob -PrinterObject $printer
+		
+		foreach ($printjob in $printjobs) {
+			Remove-PrintJob -InputObject $printjob
+		}
+	Write-Host "All print jobs cancelled for printer: $printerName"
+	}
